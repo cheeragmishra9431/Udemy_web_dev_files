ce0f4a7886c8859b70f04f75eeaddc7b24c80b19
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-10-31 19:57:45 +0530

    31st oct

---
+++ node js/my express/app.js
@@ -0,0 +1,18 @@
+var express= require('express');
+var bodyParser= require('body-parser');
+var path= require('path');
+var port= 3000;
+var app=express();
+app.use(function(req, res, next){
+    console.log("time", Date.now());
+    next();
+});
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+    res.send("hello world")
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.listen(port);
+console.log('server started '+ port);
+module.exports=app;
