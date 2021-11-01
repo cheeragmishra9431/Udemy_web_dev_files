e236ccb374e9259cbceead98b881352a4e86d7b3
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-11-01 21:55:40 +0530

    1st nov

--- node js/my express/app.js
+++ node js/my express/app.js
@@ -3,13 +3,21 @@ var bodyParser= require('body-parser');
 var path= require('path');
 var port= 3000;
 var app=express();
+app.set('views', path.join(__dirname, 'views'))
+
+var pug =require('pug');
 app.use(function(req, res, next){
     console.log("time", Date.now());
     next();
 });
+
+app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.get('/', function(req, res){
-    res.send("hello world")
+    // res.send("hello world")
+    res.render('index',{
+        title: 'hello'
+    });
 });
 app.get('/about', function(req, res){
     res.send("hello world about")
